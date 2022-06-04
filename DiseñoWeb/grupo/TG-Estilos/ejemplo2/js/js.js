fd6553a6558cf4f27d0493983d7f64146c04d809
tu_user <frvf2000@gmail.com.com>
2022-06-03 21:18:51 -0500

    nuev

--- DiseñoWeb/grupo/TG-Estilos/ejemplo2/js/js.js
+++ DiseñoWeb/grupo/TG-Estilos/ejemplo2/js/js.js
@@ -1,12 +1,10 @@
-
-
-function prender(sw){
-    var pic;
-    if (sw ==0){
-        pic="ejemplo2/img/foco_apagado.jpg"}
-    else{
-        pic = "ejemplo2/img/foco_prendido.jpg"
-    }
+ //prender
+$("#accion1").click(function () {
+    var pic = "ejemplo2/img/foco_prendido.jpg"
     document.getElementById('foco').src = pic;
-}
- 
+});
+//apagado
+$("#accion2").click(function () {
+    var pic = "ejemplo2/img/foco_apagado.jpg"
+    document.getElementById('foco').src = pic;
+}); 
--- DiseñoWeb/grupo/TG-Estilos/ejemplo4/js/js.js
+++ DiseñoWeb/grupo/TG-Estilos/ejemplo4/js/js.js
@@ -1,4 +1,30 @@
-function muestraOculta(id) {
+$(document).ready(function(){
+    // console.log('Documento listo');
+    //añadir una clase
+     $('.HH1').addClass('amarillo');
+     //eliminar clase
+     $('.parrafo').removeClass('.parrafo');
+     //intercambio, añade o borra
+     $('button').click(function(){
+         $('p').toggleClass('parrafo');
+         console.log($('p').css('font-size'));
+         console.log($('p').css('margin'));
+         $('#contenido').innerWidth(1240);
+         $('#contenido').outerHeight(3240);
+         $('p').css({'background-color':'red','font-size':'1.3em'});
+     });
+     //obtener valor propiedad
+     console.log($('p').css('font-size'));
+     console.log($('p').css('margin'));
+     //asigna o cambia valor
+     $('p').css({'background-color':'pink','font-size':'1.3em'});
+     //cambiar dimensiones
+     $('#contenido').width(1000);
+     $('#contenido').height(1000);
+     //innerWidth outerWidth innerHeight outerHeight
+ });
+
+ function muestraOculta(id) {
     var elemento = document.getElementById('contenidos_'+id);
     var enlace = document.getElementById('enlace_'+id);
   
